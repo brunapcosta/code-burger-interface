0944bd4afd704294559bce6af355f0625ef30cac
brunapcosta <brunaa.jornalismo@gmail.com>
2024-04-03 19:42:22 -0300

    change-link

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiCodeBurger = axios.create({
-    baseURL: 'https://code-burger-e6bbd6.netlify.app',
+    baseURL: 'https://burger-good.netlify.app/',
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*', 
