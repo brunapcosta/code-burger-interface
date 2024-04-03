aa20397f75cf9ce20f11441156e75251147f3e7c
brunapcosta <brunaa.jornalismo@gmail.com>
2024-04-02 21:08:40 -0300

    cors01

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 
 const apiCodeBurger = axios.create({
     baseURL: 'https://code-burger-e6bbd6.netlify.app',
-    withCredentials: true,
-    headers: {
-        "Access-Control-Allow-Origin" : "*",
-    }
+    withCredentials: false,
+    // headers: {
+    //     "Access-Control-Allow-Origin" : "*",
+    // }
     // baseURL: 'http://localhost:3001'
 })
 
@@ -13,6 +13,7 @@ apiCodeBurger.interceptors.request.use( async config => {
     const userData = await localStorage.getItem('codeburger:userData')
     const token = userData && JSON.parse(userData).token
     config.headers.authorization = `Bearer ${token}`
+    config.headers["Access-Control-Allow-Origin"] = "*"
     return config
 })
 
